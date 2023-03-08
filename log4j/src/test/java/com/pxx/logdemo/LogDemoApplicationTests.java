f0d41c3ef790ee52c7d1b04001908726693cd565
pxx <523711167@qq.com>
2023-03-08 23:29:22 +0800

    添加log4j测试代码

--- log4j/src/test/java/com/pxx/logdemo/LogDemoApplicationTests.java
+++ log4j/src/test/java/com/pxx/logdemo/LogDemoApplicationTests.java
@@ -1,26 +1,9 @@
 package com.pxx.logdemo;
 
-import com.pxx.logdemo.controller.LogController;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@Slf4j
+
 class LogDemoApplicationTests {
 
-    @Autowired
-    LogController logController;
 
-    @Test
-    void contextLoads() {
-    }
-
-    //TRACE < DEBUG < INFO < WARN < ERROR < FATAL
-    @Test
-    public void test() {
-        logController.log();
-    }
 
 }
