21df0155f8d340ad4fc8aa5db6291bf0ee7ce396
pxx <523711167@qq.com>
2023-07-03 11:47:39 +0800

    新增日志相关内容

--- log4j/src/main/java/org/pxx/logdemo/controller/LogController.java
+++ log4j/src/main/java/org/pxx/logdemo/controller/LogController.java
@@ -1,12 +1,14 @@
-package com.pxx.logdemo.controller;
+package org.pxx.logdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class LogController {
 
+    @RequestMapping("/index")
     public void log() {
         log.trace("trace");
         log.debug("debug");
--- log4j/src/test/java/org/pdaodao/AppTest.java
+++ log4j/src/test/java/org/pdaodao/AppTest.java
@@ -1,6 +1,25 @@
 package org.pdaodao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.Test;
+import org.pxx.logdemo.LogDemoApplication;
+import org.pxx.logdemo.controller.LogController;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LogDemoApplication.class)
 public class AppTest
 {
+    private final Log logger = LogFactory.getLog(LogController.class);
 
+
+    @Test
+    public void test() {
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+    }
 }
